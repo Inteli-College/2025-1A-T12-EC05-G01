4dc2e2aa4743dd53b4eef9bc0929b49df6b857e5
Davi Abreu da Silveira <davi.silveira@sou.inteli.edu.br>
2025-02-12 19:04:21 -0300

    docs: adiciona pagina inicial novamente

---
+++ docs/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Home() {
+  return (
+    <Layout title="Home" description="Hello world page">
+      <div>
+        <p>Hello world</p>
+      </div>
+    </Layout>
+  );
+}
