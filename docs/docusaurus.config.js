5153311bfbf48483263d3b4a870a9a7a219bea1c
Davi Abreu da Silveira <davi.silveira@sou.inteli.edu.br>
2025-02-14 16:21:51 -0300

    hotfix: muda a baseurl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://inteli-college.github.io/2025-1A-T12-EC05-G01',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/2025-1A-T12-EC05-G01/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
