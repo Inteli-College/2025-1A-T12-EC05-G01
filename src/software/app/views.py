2c2c61884ad1c7d4341ec24317a2fbafe7e44535
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-02-19 09:24:21 -0300

    feat: cria as rotas para renderização das páginas

--- src/software/app/views.py
+++ src/software/app/views.py
@@ -1,6 +1,39 @@
 from flask import render_template  
 from app import app
 
+#definição das rotas do projeto
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/recuperar-senha')
+def recuperar_senha():
+    return render_template('recuperarSenha.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dasboard.html')
+
+@app.route('/estoque')
+def estoque():
+    return render_template('estoque.html')
+
+@app.route('/prescricoes')
+def prescricoes():
+    return render_template('prescricoes.html')
+
+@app.route('/montagens')
+def montagens():
+    return render_template('montagens.html')
+
+@app.route('/verificacao')
+def verificacao():
+    return render_template('verificacao.html')
