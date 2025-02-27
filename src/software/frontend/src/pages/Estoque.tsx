f07e972ce033534ad419c0bc453def5ba386dd04
SophiSenne <158747104+SophiSenne@users.noreply.github.com>
2025-02-27 17:02:19 -0300

    feat: adiciona ajustes na estilização

--- src/software/frontend/src/pages/Estoque.tsx
+++ src/software/frontend/src/pages/Estoque.tsx
@@ -17,7 +17,9 @@ export default function Estoque() {
                 </h2>
 
                 <div class="alertas">
-                    <AlertaMedicamento imagem={alertaImg} medicamento={"Paracetamol 750mg"} bin={4} unidades={12}/>
+                {medicamentosReabastecer.map(medicamento => 
+                    <AlertaMedicamento imagem={alertaImg} medicamento={medicamento.medicamento} bin={medicamento.bin} classe={ClasseUnidades(medicamento.unidades)} unidades={medicamento.unidades} />
+                )}
                 </div>
             </div>
 
@@ -27,7 +29,9 @@ export default function Estoque() {
                 </h2>
 
                 <div class="alertas">
-                    <AlertaMedicamento imagem={maisImg} medicamento={"Miosan 10mg"} bin={2} unidades={"+ 60"} />
+                    {medicamentosAdicionados.map(medicamento =>
+                        <AlertaMedicamento imagem={maisImg} medicamento={medicamento.nome} bin={medicamento.bin} classe={"quantidade-remedio-add"} unidades={medicamento.unidades} />
+                    )}
                 </div>
             </div>
 
@@ -37,7 +41,9 @@ export default function Estoque() {
                 </h2>
 
                 <div class="alertas">
-                    <AlertaMedicamento imagem={menosImg} medicamento={"Paracetamol 750mg"} bin={4} unidades={"- 3"}/>
+                    {medicamentosRetirados.map(medicamento => 
+                        <AlertaMedicamento imagem={menosImg} medicamento={medicamento.nome} bin={medicamento.bin} classe={"quantidade-remedio"} unidades={medicamento.unidades} />
+                    )}
                 </div>
             </div>
 
@@ -66,7 +72,7 @@ function AdicionarRemedio() {
     );
   }
 
-function AlertaMedicamento ({imagem, medicamento, bin, unidades}){
+function AlertaMedicamento ({imagem, medicamento, bin, classe, unidades}){
     return (
         <div class="alerta-medicamento">
             <img src={imagem} alt="Alerta" width="60" />
@@ -75,7 +81,7 @@ function AlertaMedicamento ({imagem, medicamento, bin, unidades}){
                     <p class="nome-remedio">{medicamento}</p>
                     <p class="bin">Bin {bin}</p>
                 </div>
-                <p class="quantidade-remedio-critico">{unidades} unidades</p>
+                <p class={classe}>{unidades} unidades</p>
             </div>
         </div>
     );
@@ -96,18 +102,82 @@ function MedicamentoQuadro ({bin, nome, unidades}){
         <div class="medicamento-quadro">
             <p class="bin">{bin}</p>
             <p class="nome-remedio">{nome}</p>
-            <p class="quantidade-remedio">{unidades} unidades</p>
+            <p class={ClasseUnidades(unidades)}>{unidades} unidades</p>
         </div>
     );
 }
 
-var medicamentosEstoque = [{
-    bin: 1,
-    nome: "Loratadina 10mg",
+function ClasseUnidades (unidades){
+    switch (true){
+        case (unidades <= 10):
+            return "quantidade-remedio-critico";
+        case (unidades < 30):
+            return "quantidade-remedio";
+        case (unidades >= 30):
+            return "quantidade-remedio-add";
+    }
+}
+
+var medicamentosReabastecer = [{
+    medicamento: "Paracetamol 750mg",
+    bin: 4,
+    unidades: 6
+},
+{
+    medicamento: "Dipirona 1g",
+    bin: 3,
     unidades: 12
 },
 {
+    medicamento: "Loratadina 10mg",
+    bin: 1,
+    unidades: 13
+}]
+
+var medicamentosAdicionados = [{
     bin: 2,
+    nome: "Miosan 10mg", 
+    unidades: "+ 60"
+},
+{
+    bin: 5,
+    nome: "Sertralina 50mg", 
+    unidades: "+ 75"
+}]
+
+var medicamentosRetirados = [{
+    bin: 4,
+    nome: "Paracetamol 750mg",
+    unidades: "- 3"
+},
+{
+    bin: 3,
+    nome: "Dipirona 1g",
+    unidades: "- 4"
+}]
+
+var medicamentosEstoque = [{
+    bin: 1,
     nome: "Loratadina 10mg",
+    unidades: 13
+},
+{
+    bin: 2,
+    nome: "Miosan 10mg",
+    unidades: 76
+},
+{
+    bin: 3,
+    nome: "Dipirona 1g",
     unidades: 12
+},
+{
+    bin: 4,
+    nome: "Paracetamol 750mg",
+    unidades: 6
+},
+{
+    bin: 5,
+    nome: "Sertralina 50mg",
+    unidades: 81
 }]
