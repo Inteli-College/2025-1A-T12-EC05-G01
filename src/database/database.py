ae5ad597653e4bc7370a5f2d76561078495f4272
SophiSenne <Sophia.Silva@sou.inteli.edu.br>
2025-03-18 14:40:50 -0300

    feat: cria CRUD para tabela de farmaceuticos

--- src/database/database.py
+++ src/database/database.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request, Blueprint
 from .routes.logs_routes import logs_routes
 from .routes.estoque_routes import estoque_routes
+from .routes.farmaceutico_routes import farmaceutico_routes
 
 database_app = Flask(__name__)
 
 database_app.register_blueprint(logs_routes)
 database_app.register_blueprint(estoque_routes)
+database_app.register_blueprint(farmaceutico_routes)
 
 if __name__ == "__main__":
     database_app.run(host="0.0.0.0", port=3000, debug=True)
