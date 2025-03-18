d29d2b87962c7ab7d91f60149ae4fda9b5170327
SophiSenne <Sophia.Silva@sou.inteli.edu.br>
2025-03-18 13:15:01 -0300

    feat: cria CRUD para tabela de estoque

--- src/database/models/estoque.py
+++ src/database/models/estoque.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from src.database.db_conexao import Base, engine
+from ..db_conexao import Base, engine
 
 class Estoque(Base):
     __tablename__ = "estoque"
