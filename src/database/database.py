aa11e41116dcb567815633063db71a77718ba3d5
BrunoFabiani <bruno@fabiani.com.br>
2025-03-20 16:51:10 -0300

    feat: registra os blueprints da rotas da prescption_on_hold e saidas

--- src/database/database.py
+++ src/database/database.py
@@ -5,6 +5,9 @@ from .routes.farmaceutico_routes import farmaceutico_routes
 from .routes.medicamento_routes import medicamento_routes
 from .routes.medicos_routes import medicos_routes
 from .routes.paciente_routes import paciente_routes
+from .routes.prescricao_on_hold_routes import prescricao_on_hold_routes
+from .routes.saidas_routes import saida_routes
+
 
 database_app = Flask(__name__)
 
@@ -14,6 +17,8 @@ database_app.register_blueprint(farmaceutico_routes)
 database_app.register_blueprint(medicamento_routes)
 database_app.register_blueprint(medicos_routes)
 database_app.register_blueprint(paciente_routes)
+database_app.register_blueprint(prescricao_on_hold_routes)
+database_app.register_blueprint(saida_routes)
 
 if __name__ == "__main__":
     database_app.run(host="0.0.0.0", port=3000, debug=True)
