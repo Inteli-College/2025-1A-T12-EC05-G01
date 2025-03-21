c5f6d6e24e8df4dcf0bb9438b00d798c45c3296f
SophiSenne <Sophia.Silva@sou.inteli.edu.br>
2025-03-21 15:42:35 -0300

    feat: implementa bp de saidas e prescricoes

--- src/software/backend/backend.py
+++ src/software/backend/backend.py
@@ -7,6 +7,8 @@ from .routes.data.farmaceutico_routes import farmaceutico_routes
 from .routes.data.medicamento_routes import medicamento_routes
 from .routes.data.medicos_routes import medicos_routes
 from .routes.data.paciente_routes import paciente_routes
+from .routes.data.prescricao_on_hold_routes import prescricao_on_hold_routes
+from .routes.data.saidas_routes import saida_routes
 from .routes.authentication.authentication_routes import authentication_routes
 from software.backend.config import Config 
 from dotenv import load_dotenv
@@ -28,6 +30,8 @@ backend_app.register_blueprint(medicamento_routes)
 backend_app.register_blueprint(medicos_routes)
 backend_app.register_blueprint(paciente_routes)
 backend_app.register_blueprint(authentication_routes)
+backend_app.register_blueprint(prescricao_on_hold_routes)
+backend_app.register_blueprint(saida_routes)
 
 if __name__ == "__main__":
     backend_app.run(host="0.0.0.0", port=BACKEND_PORT, debug=True)
