89792c702109713f459e3a411f2741bacb895109
Daniel Parente Pessoa Dias <daniel.dias@sou.inteli.edu.br>
2025-03-24 17:15:35 -0300

    fix: adiciona novo atributo status no model prescricao_aceita

--- src/software/database/schema.sql
+++ src/software/database/schema.sql
@@ -84,6 +84,7 @@ CREATE TABLE prescricao_aceita (
   id_prescricao_on_hold INTEGER NOT NULL,  -- Relaciona diretamente com a prescrição inicial
   id_farmaceutico INTEGER NOT NULL,
   data_validacao TEXT DEFAULT (datetime('now')),
+  status_prescricao TEXT DEFAULT 'aguardando_separacao',  -- SQLite não suporta CHECK ENUM, validação deve ser na aplicação
   FOREIGN KEY (id_prescricao_on_hold) REFERENCES prescricao_on_hold(id) ON DELETE CASCADE,
   FOREIGN KEY (id_farmaceutico) REFERENCES farmaceutico(id) ON DELETE CASCADE
 );
