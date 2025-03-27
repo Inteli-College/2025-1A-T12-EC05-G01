98f93cd84cce42f357efca3f4e3aca24c593f54f
Davi Abreu da Silveira <davi.silveira@sou.inteli.edu.br>
2025-03-27 17:54:39 -0300

    hot-fix: endpoint

--- src/software/frontend/src/pages/Cadastro.tsx
+++ src/software/frontend/src/pages/Cadastro.tsx
@@ -26,7 +26,7 @@ const Cadastro = () => {
       
       // Se for médico, cria registro na tabela de medicos com CRM, senão, cria registro em farmaceutico
       if (isDoctor) {
-        await axios.post(`${API_BASE_URL}/medicos/create`, { email, nome, crm });
+        await axios.post(`${API_BASE_URL}/medico/create`, { email, nome, crm });
       } else {
         await axios.post(`${API_BASE_URL}/farmaceutico/create`, { email, nome });
       }
